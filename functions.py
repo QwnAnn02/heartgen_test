1bd8a17822a6867a2678cda634719da5818b71cd
QwnAnn02 <anan.philips02@gmail.com>
2024-01-19 12:04:20 +0530

    added error handling and cleaned up the code

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from jinja2 import Template
 import re
-
+import sys 
 
 
 def validate_field_names(df):
@@ -13,21 +13,29 @@ def validate_field_names(df):
                          'cmdb ci uid', 'cmdb ci parent name', 'cmdb ci parent uid',
                          'cmdb event category','mode', 'timeout', 'wait', 'tags'
                          ]
-    
-    for column in expected_columns:
-        if column not in df.columns:
-            raise ValueError(f"Column '{column}' is missing!.")   
+    try:
 
-    unexpected_columns = set(df.columns) - set(expected_columns)
-    if unexpected_columns:
-      raise ValueError(f"Unexpected columns present: {', '.join(unexpected_columns)}") #re-direct for input correction
+        for column in expected_columns:
+            if column not in df.columns:
+                raise ValueError(f"Column '{column}' is missing!.")   
+
+        unexpected_columns = set(df.columns) - set(expected_columns)
+        if unexpected_columns:
+            raise ValueError(f"Unexpected columns present: {', '.join(unexpected_columns)}") #re-direct for input correction?
+    except ValueError as ve:
+        print(f"Error in field names: {ve}")
+        sys.exit(1)
         
 def check_mandatory_fields(df):
     mandatory_fields = ['type', 'id', 'name','hosts']
+    try:
 
-    for field in mandatory_fields:
-        if df[field].isnull().any():
-            raise ValueError(f"Mandatory field '{field}' contains null values.") #re-direct for input correction and append it in the excel sheet or should the user go back to the excel sheet?
+        for field in mandatory_fields:
+            if df[field].isnull().any():
+                raise ValueError(f"Mandatory field '{field}' contains null values.") #re-direct for input correction and append it in the excel sheet or should the user go back to the excel sheet?
+    except ValueError as ve:
+        print(f"Mandatory field(s) missing : {ve}")
+        sys.exit(1)
         
 
 # def check_data_types(df): #validate hosts and country and city drop-down
@@ -58,8 +66,12 @@ def fix_trailing_spaces(df):
 def validate_ip_addresses(df):
     ipv4_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
     ipv6_pattern = re.compile(r'^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$')
+    try:
 
-    for _, row in df.iterrows():
-        for host in row['hosts'].split(', '):
-            if not ipv4_pattern.match(host) and not ipv6_pattern.match(host):
-                raise ValueError(f"Invalid IP address format for host '{host}' in row {row.name}") #re-direct for input correction
+        for _, row in df.iterrows():
+            for host in row['hosts'].split(', '):
+                if not ipv4_pattern.match(host) and not ipv6_pattern.match(host):
+                    raise ValueError(f"Invalid IP address format for host '{host}' in row {row.name}") #re-direct for input correction
+    except ValueError as ve:
+        print("Error in IP address format : {ve}")
+        sys.exit(1)
--- main_run02.py
+++ main_run02.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from jinja2 import Template
-from functions import validate_field_names, check_mandatory_fields , check_data_types, fix_trailing_spaces, validate_ip_addresses
+from functions import validate_field_names, check_mandatory_fields , fix_trailing_spaces, validate_ip_addresses
 from yaml_template import yaml_template
 from datetime import datetime
 import os
 
 def main():
     # Read Excel data into a Pandas DataFrame
-    excel_file = 'heartbeat base xlsx template_copy(2).xlsx'  
-    df = pd.read_excel(excel_file, engine='openpyxl', index_col=None)
-    print(df.columns)
+    input_excel = 'heartbeat_base_input_template.xlsx'  
+    df = pd.read_excel(input_excel, engine='openpyxl', index_col=None)
+    #print(df.columns)
 
     # Validate field names
     validate_field_names(df)
@@ -18,7 +18,7 @@ def main():
     check_mandatory_fields(df)
 
     # Check data types
-    check_data_types(df)
+    #check_data_types(df)
 
     # Fix trailing spaces
     df = fix_trailing_spaces(df)
@@ -26,10 +26,19 @@ def main():
     # Validate IP address format
     validate_ip_addresses(df)
 
-    #appending a date column in the master excel
-    date_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    backup_excel_file = f'input_data_backup_{date_now}.xlsx'
-    df.to_excel(backup_excel_file, index=False, engine='openpyxl')
+    now = datetime.now()
+
+    backup_excel_file = 'backup_heartbeat_config.xlsx'
+    if os.path.exists(backup_excel_file):
+        backup_df = pd.read_excel(backup_excel_file, engine='openpyxl', index_col=None)
+        backup_df = pd.concat([backup_df, df], ignore_index=True)
+    else:
+        backup_df = df.copy()
+        #add a column with current date and time
+    backup_df['date']= now.strftime('%d-%m-%y')
+    backup_df['time']= now.strftime('%H:%M:%S')
+
+    backup_df.to_excel(backup_excel_file, index=False, engine='openpyxl')
 
 
     # Create Jinja2 template object
@@ -39,17 +48,17 @@ def main():
     yaml_data = template.render(data=df)
 
     # Save YAML data to a file
-    yaml_file = 'heartbeat yaml output_test02.yaml'
+    yaml_file = 'heartbeat yaml config file.yaml'
     with open(yaml_file, 'w') as f:
        f.write(yaml_data)
 
     print(f"YAML data has been successfully written to {yaml_file}.")
 
-    # Append data to master Excel sheet
-    master_excel_file = 'master_data.xlsx'
+    # Append data to Master Excel sheet
+    master_excel_file = 'heartbeat_gen_master_data.xlsx'
     if os.path.exists(master_excel_file):
         # Read existing master data
-        master_df = pd.read_excel(master_excel_file, engine='openpyxl', index_col=None)
+        master_df = pd.read_excel(master_excel_file, engine='openpyxl', index_col=None) #identifying column(s)?
         # Append the new data to the master data
         master_df = pd.concat([master_df, df], ignore_index=True)
     else:
@@ -57,14 +66,19 @@ def main():
         master_df = df.copy()
 
     # Add a date column with the current date
-    master_df['date'] = date_now
+    master_df['date'] = now.strftime('%d-%m-%y')
+    master_df['time'] = now.strftime('%H:%M:%S')
 
     # Save the updated master data
     master_df.to_excel(master_excel_file, index=False, engine='openpyxl')
 
     # Delete contents of the input Excel sheet
-    df.iloc[0:0].to_excel(excel_file, index=False, engine='openpyxl')
+    
+    # Delete contents of the input Excel sheet, keeping only the column headers
+    df.iloc[2:, :].to_excel(input_excel, index=False, engine='openpyxl')
 
+
+        
     print(f"Data has been successfully appended to {master_excel_file} and input Excel sheet has been cleared.")
 
 if __name__ == "__main__":
